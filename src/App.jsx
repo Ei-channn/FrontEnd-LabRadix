fb5d388c2ab6bb1bf6c26d860f02622ae2bb9695
Ei_chann <rifqi070409@gmail.com>
2026-03-24 18:59:46 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router";
+import './App.css'
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/login" element={<Login />}/> */}
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/admin/dasboard.jsx
@@ -0,0 +1,14 @@
+function Dashboard() {
+
+    console.log("TOKEN:", localStorage.getItem("token"));
+
+    return (
+        <div>
+            <div>
+                 
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
